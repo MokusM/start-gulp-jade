56752f00f75792596de7c776945ac0af0849de42
moiseev.sliceplanet@gmail.com <moiseev.sliceplanet@gmail.com>
2020-03-21 23:04:27 +0200

    change jade on pug

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,108 +1,102 @@
 $(window).on('load', function () {
-	if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+	if (/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent)) {
 		$('body').addClass('ios');
-	} else{
+	} else {
 		$('body').addClass('web');
 	};
-	$('body').removeClass('loaded'); 
+	$('body').removeClass('loaded');
 });
+
 /* viewport width */
-function viewport(){
-	var e = window, 
+function viewport() {
+	var e = window,
 		a = 'inner';
-	if ( !( 'innerWidth' in window ) )
-	{
+	if (!('innerWidth' in window)) {
 		a = 'client';
 		e = document.documentElement || document.body;
 	}
-	return { width : e[ a+'Width' ] , height : e[ a+'Height' ] }
+	return { width: e[a + 'Width'], height: e[a + 'Height'] }
 };
 /* viewport width */
-$(function(){
-	/* placeholder*/	   
-	$('input, textarea').each(function(){
- 		var placeholder = $(this).attr('placeholder');
- 		$(this).focus(function(){ $(this).attr('placeholder', '');});
- 		$(this).focusout(function(){			 
- 			$(this).attr('placeholder', placeholder);  			
- 		});
- 	});
+$(function () {
 	/* placeholder*/
-
-	$('.button-nav').click(function(){
-		$(this).toggleClass('active'), 
-		$('.main-nav-list').slideToggle(); 
+	$('input, textarea').each(function () {
+		var placeholder = $(this).attr('placeholder');
+		$(this).focus(function () { $(this).attr('placeholder', ''); });
+		$(this).focusout(function () {
+			$(this).attr('placeholder', placeholder);
+		});
+	});
+	/* placeholder*/
+	/*button open main nav begin*/
+	$('.js-button-nav').click(function () {
+		$(this).toggleClass('active'),
+			$('.main-nav-list').slideToggle();
 		return false;
 	});
-	
-	/* components */
-	
-	/*
-	
-	if($('.styled').length) {
-		$('.styled').styler();
+
+	/*button open main nav end*/
+
+	/* components begin*/
+	/*	
+	if ($('.js-img').length) {
+		var jsImg = $(".js-img");
+		new LazyLoad(jsImg, {
+				root: null,
+				rootMargin: "0px",
+				threshold: 0
+		});
 	};
-	if($('.fancybox').length) {
-		$('.fancybox').fancybox({
-			margon  : 10,
+	if ($('.js-swiper').length) {
+		var mySwiper = new Swiper('.js-swiper', {
+				loop: true,
+				slidesPerView: 1,
+				noSwiping: true,
+				autoplay: {
+						delay: 1000,
+						disableOnInteraction: false,
+				},
+				breakpoints: {
+						1024: {
+								slidesPerView: 2
+						}
+				}
+		})
+	}; 
+
+	if($('.js-styled').length) {
+		$('.js-styled').styler();
+	};
+	if($('.js-fancybox').length) {
+		$('.js-fancybox').fancybox({
+			margin  : 10,
 			padding  : 10
 		});
 	};
-	if($('.slick-slider').length) {
-		$('.slick-slider').slick({
-			dots: true,
-			infinite: false,
-			speed: 300,
-			slidesToShow: 4,
-			slidesToScroll: 4,
-			responsive: [
-				{
-				  breakpoint: 1024,
-				  settings: {
-					slidesToShow: 3,
-					slidesToScroll: 3,
-					infinite: true,
-					dots: true
-				  }
-				},
-				{
-				  breakpoint: 600,
-				  settings: "unslick"
-				}				
-			]
-		});
-	};
-	if($('.scroll').length) {
-		$(".scroll").mCustomScrollbar({
+	
+	if($('.js-scroll').length) {
+		$(".js-scroll").mCustomScrollbar({
 			axis:"x",
 			theme:"dark-thin",
 			autoExpandScrollbar:true,
 			advanced:{autoExpandHorizontalScroll:true}
 		});
-	};
-	
+	};	
 	*/
-	
-	/* components */
-	
-	
-
+	/* components end*/
 });
 
-var handler = function(){
-	
-	var height_footer = $('footer').height();	
-	var height_header = $('header').height();		
-	//$('.content').css({'padding-bottom':height_footer+40, 'padding-top':height_header+40});
-	
-	
+var handler = function () {
+	var height_footer = $('footer').height();
+	var height_header = $('header').height();
+	//$('.content').css({'padding-bottom':height_footer+40, 'padding-top':height_header+40});	
+
 	var viewport_wid = viewport().width;
 	var viewport_height = viewport().height;
-	
+
 	if (viewport_wid <= 991) {
-		
+
 	}
-	
 }
 $(window).bind('load', handler);
 $(window).bind('resize', handler);
--- gulpfile.js
+++ gulpfile.js
@@ -1,49 +1,50 @@
 
-var gulp       = require('gulp'), // Подключаем Gulp
-	sass         = require('gulp-sass'), //Подключаем Sass пакет,
-	browserSync  = require('browser-sync'), // Подключаем Browser Sync
-	concat       = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
+var gulp = require('gulp'), // Подключаем Gulp
+	sass = require('gulp-sass'), //Подключаем Sass пакет,
+	browserSync = require('browser-sync'), // Подключаем Browser Sync
+	//concat = require('gulp-concat'), // Подключаем gulp-concat (для конкатенации файлов)
 	/*uglify       = require('gulp-uglifyjs'),*/ // Подключаем gulp-uglifyjs (для сжатия JS)
 	/*cssnano      = require('gulp-cssnano'),*/ // Подключаем пакет для минификации CSS
-	rename       = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
-	del          = require('del'), // Подключаем библиотеку для удаления файлов и папок
-	imagemin     = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
-	pngquant     = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
-	cache        = require('gulp-cache'), // Подключаем библиотеку кеширования
+	rename = require('gulp-rename'), // Подключаем библиотеку для переименования файлов
+	del = require('del'), // Подключаем библиотеку для удаления файлов и папок
+	imagemin = require('gulp-imagemin'), // Подключаем библиотеку для работы с изображениями
+	pngquant = require('imagemin-pngquant'), // Подключаем библиотеку для работы с png
+	cache = require('gulp-cache'), // Подключаем библиотеку кеширования
 	plumber = require('gulp-plumber'), // Чтоб при ошибке не падал сервер
 	autoprefixer = require('gulp-autoprefixer'),// Подключаем библиотеку для автоматического добавления префиксов
 	spritesmith = require('gulp.spritesmith'), //Для автосборки спрайта
-	jade        = require('gulp-jade'), // Компилятор jade
-	pipeErrorStop = require('pipe-error-stop'),
+	pug = require('gulp-pug'), // Компилятор pug
 	sourcemaps = require('gulp-sourcemaps'); //Что б в режиме разработчика показывало норм стили
 
 
-gulp.task('sass', function(){ // Создаем таск Sass
+gulp.task('sass', function () { // Создаем таск Sass
 	return gulp.src('app/scss/**/*.scss') // Берем источник
 		.pipe(sourcemaps.init())
 		.pipe(plumber())
-		.pipe(sass({outputStyle: 'compact'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass				
+		.pipe(sass({ outputStyle: 'compact' }).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass				
 		.pipe(autoprefixer(['last 10 versions', '> 1%', 'ie 9', 'ie 10'], { cascade: true })) // Создаем префиксы
 		.pipe(sourcemaps.write('.'))
 		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
-		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
-});
-
-
-gulp.task('jade',  function() {
-  return gulp.src('app/jade/**/*.jade')// Берем источник
-  	.pipe(plumber())
-    .pipe(jade({pretty: true}))
-    .pipe(gulp.dest('app/'))
-    .pipe(pipeErrorStop())
-    .pipe(browserSync.stream());
+		.pipe(browserSync.reload({ stream: true })) // Обновляем CSS на странице при изменении
 });
 
 
 
+gulp.task('pug', function () {
+	gulp.src('app/pug/*.+(jade|pug)')
+		.pipe(pug({ pretty: '\t' }))
+		.on('error', function (err) {
+			console.log(err); // Would like to catch the error here
+		})
+		.pipe(gulp.dest('app/'))
+		.pipe(browserSync.reload({ stream: true }))
+});
 
 
-gulp.task('browser-sync', function() { // Создаем таск browser-sync
+
+
+
+gulp.task('browser-sync', function () { // Создаем таск browser-sync
 	browserSync({ // Выполняем browserSync
 		server: { // Определяем параметры сервера
 			baseDir: 'app' // Директория для сервера - app
@@ -53,74 +54,74 @@ gulp.task('browser-sync', function() { // Создаем таск browser-sync
 });
 
 gulp.task('sprite', function () {
-  var sprite= gulp.src('app/img/icons/*.png').pipe(spritesmith({
-    imgName: '../img/sprite.png',
-    cssName: '_sprite.scss',
-    cssFormat: 'scss',
-    algoritm: 'binary-tree',
-    padding: 5
-  }));
-  sprite.img.pipe(rename('sprite.png')).pipe(gulp.dest('app/img/'));
-  sprite.css.pipe(gulp.dest('app/scss/utils/'));
+	var sprite = gulp.src('app/img/icons/*.png').pipe(spritesmith({
+		imgName: '../img/sprite.png',
+		cssName: '_sprite.scss',
+		cssFormat: 'scss',
+		algoritm: 'binary-tree',
+		padding: 5
+	}));
+	sprite.img.pipe(rename('sprite.png')).pipe(gulp.dest('app/img/'));
+	sprite.css.pipe(gulp.dest('app/scss/utils/'));
 });
 /* если нужно обединить все скрипты в один розкоментировать и в шаблоне подключить один файл*/
 /*gulp.task('scripts', function() {
 	return gulp.src([*/
-		/*'app/js/jquery-3.0.0.min.js',
-		'app/js/jquery-migrate-1.4.1.min.js',
-		'app/js/components/jquery.fancybox.js',
-		'app/js/components/jquery.formstyler.js',
-		'app/js/components/jquery.mCustomScrollbar.js',
-		'app/js/components/slick.js'*///тут нужно подключать библиотеки
-		/*])
-	.pipe(concat('scripts.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('app/js'))
-	.pipe(browserSync.reload({stream: true}));
+/*'app/js/jquery-3.0.0.min.js',
+'app/js/jquery-migrate-1.4.1.min.js',
+'app/js/components/jquery.fancybox.js',
+'app/js/components/jquery.formstyler.js',
+'app/js/components/jquery.mCustomScrollbar.js',
+'app/js/components/slick.js'*///тут нужно подключать библиотеки
+/*])
+.pipe(concat('scripts.min.js'))
+.pipe(uglify())
+.pipe(gulp.dest('app/js'))
+.pipe(browserSync.reload({stream: true}));
 });*/
 
 
-gulp.task('watch', ['browser-sync', 'sprite', 'jade'], function() {
+gulp.task('watch', ['browser-sync', 'sprite', 'pug'], function () {
 	gulp.watch('app/scss/**/*.scss', ['sass']); // Наблюдение за sass файлами в папке sass
 	gulp.watch(['app/img/icons/**/*.png'], ['sprite']);
 	gulp.watch('app/*.html', browserSync.reload); // Наблюдение за HTML файлами в корне проекта
-	gulp.watch('app/jade/**/*.jade', ['jade']); // Наблюдение за jade файлами в корне проекта
+	gulp.watch('app/pug/**/*.pug', ['pug']); // Наблюдение за pug файлами в корне проекта
 	gulp.watch('app/js/**/*.js', browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
 
-gulp.task('clean', function() {
+gulp.task('clean', function () {
 	return del.sync('dist'); // Удаляем папку dist перед сборкой
 });
 
-gulp.task('img', function() {
+gulp.task('img', function () {
 	return gulp.src('app/img/**/*') // Берем все изображения из app
 		.pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
 			interlaced: true,
 			progressive: true,
-			svgoPlugins: [{removeViewBox: false}],
+			svgoPlugins: [{ removeViewBox: false }],
 			use: [pngquant()]
 		})))
 		.pipe(gulp.dest('dist/img')); // Выгружаем на продакшен
 });
 
-gulp.task('build', ['clean', 'img', 'sass', 'jade'], function() {
+gulp.task('build', ['clean', 'img', 'sass', 'pug'], function () {
 
 	var buildCss = gulp.src([ // Переносим библиотеки в продакшен
 		'app/css/style.css'
-		])
-	.pipe(gulp.dest('dist/css'))
+	])
+		.pipe(gulp.dest('dist/css'))
 
 	var buildFonts = gulp.src('app/scss/**/*') // Переносим scss в продакшен
-	.pipe(gulp.dest('dist/scss'))
+		.pipe(gulp.dest('dist/scss'))
 
 	var buildFonts = gulp.src('app/fonts/**/*') // Переносим шрифты в продакшен
-	.pipe(gulp.dest('dist/fonts'))
+		.pipe(gulp.dest('dist/fonts'))
 
 	var buildJs = gulp.src('app/js/**/*') // Переносим скрипты в продакшен
-	.pipe(gulp.dest('dist/js'))
+		.pipe(gulp.dest('dist/js'))
 
 	var buildHtml = gulp.src('app/*.html') // Переносим HTML в продакшен
-	.pipe(gulp.dest('dist'));
+		.pipe(gulp.dest('dist'));
 
 });
 
